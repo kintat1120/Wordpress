c1bcd0b71bf3667e1af051beee281575f20b71f5
kintat1120 <52268674+kintat1120@users.noreply.github.com>
2021-07-15 18:26:01 +0800

    finish blog page, start post page(15/07/2021))

--- starter-theme-1.x/single.php
+++ starter-theme-1.x/single.php
@@ -11,9 +11,39 @@
 
 $context         = Timber::context();
 $timber_post     = Timber::get_post();
+//Get post information
 $context['post'] = $timber_post;
+//Get post tags
 $context['tags'] = Timber::get_terms('post_tag');
+//var_dump($context['post']->tags);
 //var_dump($context['post']);
+//$tag_names = array("移民須知");
+/*
+foreach($context['post']->tags as $tag){
+    array_push($tag_names, $tag->name);
+    //var_dump($tag->name);
+}*/
+//var_dump($tag_names);
+
+/*$context['relatedPost'] = new Timber\PostQuery(array(
+    'post_type' => 'post',
+    'post_tags'     => array( $tag_names ),
+    'posts_per_page' => 3, //how many recent pages want to show
+));
+var_dump($context['relatedPost']);
+*/
+
+//show 2 more posts
+$context['morePost'] = new Timber\PostQuery(array(
+    'post_type' => 'post',
+    'posts_per_page' => 2, //how many recent pages want to show
+));
+
+//get all posts
+$context['allPost'] = new Timber\PostQuery(array(
+    'post_type' => 'post'
+));
+
 if ( post_password_required( $timber_post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
 } else {
